d7bd21a53565de1c70c3e3d0e68ef57716ed23a6
annmartyn <yutamar@yandex.ru>
2021-09-25 20:57:21 +0300

    done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,19 +1,24 @@
 const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 
+let checkWin = (message) => {
+    if (dead.textContent === '10' || lost.textContent === '5') {
+        dead.textContent = 0;
+        lost.textContent = 0;
+        alert(message);
+    }
+}
+
 let elements = [];
 for (let i = 1; i < 10; i++) {
     hole = document.getElementById(String('hole' + i));
     hole.onclick = function () {
-        if (hole.className.includes( 'hole_has-mole ')) {
+        if (hole.classList.contains('hole_has-mole')) {
             dead.textContent = Number(dead.textContent) + 1;
+            checkWin('You won!');
         } else {
             lost.textContent = Number(lost.textContent) + 1;
+            checkWin('You lost!');
         }
     }
-}
-
-if (dead.textContent || lost.textContent) {
-    dead.textContent = 0;
-    lost.textContent = 0;
 }
